702e7728477f18bff67053c7dc76d842e22c5662
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2021-12-27 10:08:45 +0000

    [Add]画像投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,18 @@
 class BooksController < ApplicationController
-  
+
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -14,5 +20,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
