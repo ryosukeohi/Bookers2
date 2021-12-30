bc174ab7e5a13eddf67c3d3327a12798e2ef5d75
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2021-12-30 07:39:18 +0000

    [Add]ユーザー一覧画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 end
