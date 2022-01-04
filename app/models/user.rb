6f42976af2aed263bfafd7b8383ac55caccac0a3
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-01-04 16:13:43 +0000

    [Add]プロフィール編集エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
    attachment :profile_image
 
-   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 200 }
+   validates :name, uniqueness: true, length: { minimum: 2, maximum: 200 }
    validates :introduction, length: { maximum: 50 }
 end
