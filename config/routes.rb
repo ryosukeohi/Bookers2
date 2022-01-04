ca458702c750f4ccf70f7a55f52ad7567ae41972
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-01-04 16:04:22 +0000

    [Add]投稿編集エラーメッセージ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about' => 'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :update, :index, :new]
 end
