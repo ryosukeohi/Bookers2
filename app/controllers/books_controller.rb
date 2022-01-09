ffb90b4f092752ec9d58a776f113cbdfe74b9968
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-01-09 04:14:15 +0000

    [Fix]投 稿詳細からプロフィール画面へのリンク、edit,destroyの表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,11 @@ before_action :baria_user, only: [:edit, :destroy, :update]
     if @book.save
     flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
-  else
-    @books = Book.all
-    @user = User.find(current_user.id)
-    render :index
-  end
+    else
+   　　　 @books = Book.all
+       @user = User.find(current_user.id)
+       render :index
+    end
   end
 
   def edit
@@ -37,7 +37,7 @@ before_action :baria_user, only: [:edit, :destroy, :update]
   def show
     @books = Book.find(params[:id])
     @book = Book.new
-    @user = User.find(current_user.id)
+    @user = @books.user
   end
 
   def destroy
@@ -56,6 +56,6 @@ before_action :baria_user, only: [:edit, :destroy, :update]
     unless Book.find(params[:id]).user_id.to_i == current_user.id
         redirect_to books_path
     end
-   end
+  end
 
 end
