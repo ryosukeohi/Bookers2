d6358d5e323b79c2c4d6777f51ba5c7ff9a5cfc8
ryosukeohi <95561370+ryosukeohi@users.noreply.github.com>
2022-01-10 08:24:06 +0000

    [Fix]投稿編集ボタン、新規投稿ボタン修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root to: 'homes#top'
   get '/home/about' => 'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :date]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :update, :index, :new]
 end
